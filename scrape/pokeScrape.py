76ecf648d6015520a394a95ba5aa139267dc07a7
Ryan Bailey <ryan.n.bailey@wsu.edu>
2020-06-27 20:16:18 -0700

    progress on scrapper

--- scrape/pokeScrape.py
+++ scrape/pokeScrape.py
@@ -6,5 +6,23 @@ rillatest = requests.get('https://www.serebii.net/pokedex-swsh/', auth=('user', 
 soup = BeautifulSoup(rillatest.text, features="html.parser")
 #print(r.text)
 print(soup.title)
-print(soup.find("select"))
+list1 = []
+list1 = (soup.findAll("select"))
+n = 0
+#print(soup.findAll("select"))
 #print(soup)
+for j in list1:
+    for g in j:
+        d = str(g)
+        sl = str(g)[29:]
+        print(d[29:29 + int(sl.find("/"))])
+
+        n += 1
+    #i = str(j)
+    #stri = i[28:]
+    #print(j)
+
+    #print("ENDDDD\n")
+    #print(i[28:i.find("/")])
+    #n += 1
+print(n)
